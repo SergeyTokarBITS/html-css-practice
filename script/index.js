43f28cb767cb8de7b80765a76ee2ba4b01ad6ff2
Sergey Tokar <s.tokar@belitsoft.com>
2021-12-08 18:43:18 +0300

    refactor: change slider order

--- script/index.js
+++ script/index.js
@@ -1,36 +1,10 @@
 import {breakpoints} from "./breakpoints.js";
 
 $(document).ready(function () {
-    $('.review-list__content').slick({
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: '<button type="button" class="review-slider-prev-arrow custom-arrow"></button>',
-        nextArrow: '<button type="button" class="review-slider-next-arrow custom-arrow"></button>',
-        responsive: [
-            {
-                breakpoint: breakpoints.xl,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: breakpoints.md,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            },
-        ]
-    });
     $('.brands-logo').slick({
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 7000,
+        autoplaySpeed: 10000,
         speed: 500,
         arrows: false,
         slidesToShow: 4,
@@ -59,5 +33,30 @@ $(document).ready(function () {
             },
         ]
     });
-
+    $('.review-list__content').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="review-slider-prev-arrow custom-arrow"></button>',
+        nextArrow: '<button type="button" class="review-slider-next-arrow custom-arrow"></button>',
+        responsive: [
+            {
+                breakpoint: breakpoints.xl,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: breakpoints.md,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
+    });
 });
